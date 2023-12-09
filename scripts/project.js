0c1a01473c47d525ce019371671b752c6e7392bf
Awesomeness7567 <joh22044@byui.edu>
2023-12-09 14:39:23 -0700

    project

--- scripts/project.js
+++ scripts/project.js
@@ -1,9 +1,11 @@
 function getQuote() {
-    const url = "https://api.quotable.io/random";
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        const quote = data.content;
-        document.getElementById("quote").innerHTML = `"${quote}"`;
-      });
+  const url = "./quotes.json";
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const quotes = data.quotes;
+      const randomIndex = Math.floor(Math.random() * quotes.length);
+      const quote = quotes[randomIndex].quote;
+      document.getElementById("quote").innerHTML = `"${quote}"`;
+    });
 }
